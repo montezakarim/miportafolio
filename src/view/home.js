df77755e4503eefcd2f3824ed8fd8428bdc8a1e8
montezakarim <kmonteza@hotmail.com>
2019-10-08 22:16:44 -0500

    portafolio colors cv

--- src/view/home.js
+++ src/view/home.js
@@ -4,24 +4,20 @@ export default () => {
      <section id="inicio"> 
        <div class="intro">
         <div class="animationInit">
-          <svg viewBox="82 -45 510 102">
+          <svg viewBox="82 -45 600 200">
           <text text-anchor="middle" x="60%" y="50%">Karim</text>
         </svg>
         </div>
         <div class="apellido">
           <svg viewBox="35-30 700 92">
-          <text text-anchor="middle" x="55%" y="50%">Monteza Montenegro</text>
-        </svg>
+            <text text-anchor="middle" x="55%" y="50%">Monteza Montenegro</text>
+          </svg>
         </div>
         <br>
         <br>
-        <h5> Desarrolladora Front-end </h5>     
+        <h5 class="developer"> Desarrolladora Front-end </h5>     
       </div>
     </section>`;
   containerHome.innerHTML = templateHome;
   return containerHome;
 };
-
-/* <div class="foto">
-<img src="img/perfil1.jpg" >
-</div> */
--- src/view/portafolio.js
+++ src/view/portafolio.js
@@ -1,33 +1,32 @@
+
 export default () => {
   const containerPortafolio = document.createElement('div');
   const templatePortafolio = `
   <section id="portafolio">
-    <div class="carousel">            
+    <div class="carousel">    
       <a class="carousel-item" href="https://github.com/montezakarim/LIM010-Cipher" target="_blank">
         <div class="overlay">
           <h5>Safe Text </h5>
-          <p>Funcionalidad, diseño y desarrollo, animaciones, HTML, CSS y responsive.<span>Ver más ></span></p>
+          <p>Funcionalidad, diseño y desarrollo, animaciones, HTML, CSS y responsive.<span> Ver más </span></p>
         </div>
         <img src="img/cipher.JPG">
       </a>
-      <br>
-      <br>
       <a class="carousel-item" href="https://github.com/montezakarim/LIM010-data-lovers" target="_blank">
-        <div class="overlay">
-          <h5>Sitio Web Pokemon Go Data</h5>
-          <p>JavaScript, diseño layout y desarrollo, HTML, CSS, resposivo. <span>Ver más ></span></p>
-        </div>
-        <img src="img/pokemonGo.JPG">
-      </a>     
-      <br>
-      <br>
-      <a class="carousel-item" href="https://github.com/montezakarim/LIM010-social-network" target="_blank">
-        <div class="overlay">
-          <h5>Sitio Web Easy Start Red Social</h5>
-          <p>Diseño de layout, animaciones, HTML, CSS, SPA, diseño responsivo, JavaScript, Firebase. <span>Ver más ></span></p>
-        </div>
-        <img src="img/easystar.JPG">
-      </a>
+      <div class="overlay">
+        <h5>Sitio Web Pokemon Go Data</h5>
+        <p>JavaScript, diseño layout y desarrollo, HTML, CSS, resposivo. <span> Ver más</span></p>
+      </div>
+      <img src="img/pokemonGo.JPG">
+    </a>
+    <br>
+    <br>
+    <a class="carousel-item" href="https://github.com/montezakarim/LIM010-social-network" target="_blank">
+      <div class="overlay">
+        <h5>Sitio Web Easy Start Red Social</h5>
+        <p>Diseño de layout, animaciones, HTML, CSS, SPA, diseño responsivo, JavaScript, Firebase. <span> Ver más</span></p>
+      </div>
+      <img src="img/easystar.JPG">
+    </a>
     </div>
   </section>`;
   containerPortafolio.innerHTML = templatePortafolio;
