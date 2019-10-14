9afef8bdc22f1dd6dab55328a22a71e2cf79c55d
montezakarim <kmonteza@hotmail.com>
2019-10-13 22:29:56 -0500

    feedback mentora

--- src/view/aboutme.js
+++ src/view/aboutme.js
@@ -9,14 +9,10 @@ export default () => {
       <h4>Sobre Mí</h4>
       <br>
       <p>
-        Soy Karim Monteza, egresada de Laboratoria como Front-end Developer, con background en Administración de empresas.
-        Apasionada, creativa, organizada y responsable. En el trayecto en Laboratoria he desarollado varios proyectos de desarrollo web, 
-        he aprendido el manejo de diferentes herramientas y he fortalecido mis habilidades en comunicaión, trabajo colaborativo y metodologías ágiles. 
-        Me encanta el mundo tech, solucionar problemas y aprender cada día. Desde a que empecé a programar y a construir plataformas web me motiva a
-        estar en constante aprendizaje, especializarme y además de mejorar mis habilidades asumiendo nuevos retos profesionales. 
-        Más adelante quiero ser desarrolladora full-stack y liderar proyectos. 
+        Soy Karim Monteza, egresada de Laboratoria como Front-end Developer, con background en Administración de empresas. Apasionada, creativa, organizada y responsable. 
+        En el trayecto en Laboratoria he desarrollado varios proyectos de desarrollo web, he aprendido el manejo de diferentes herramientas y he fortalecido mis habilidades en comunicación, trabajo colaborativo y metodologías ágiles. Me encanta el mundo tech, solucionar problemas y aprender cada día. Desde que empecé a programar y a construir plataformas web me motiva a estar en constante aprendizaje, especializarme y además de mejorar mis habilidades asumiendo nuevos retos profesionales. Más adelante quiero ser desarrolladora full-stack y liderar proyectos.
         <br>
-        Me encataría ser parte de un equipo que busca dar las mejores soluciones a los problemas y mejorar la calidad de vida de las personas para un mundo mejor.
+        Me encantaría ser parte de un equipo que busca dar las mejores soluciones a los problemas y mejorar la calidad de vida de las personas para un mundo mejor.
       </p>
       <br>
       <h5>Habilidades</h5>
@@ -29,9 +25,7 @@ export default () => {
           <a href="#"><i class="fab fa-react"></i></a>
           <a href="#"><i class="fab fa-github"></i></a>  
           <a href="#"><i class="fas fa-users"></i></a>
-          <a href="#"><i class="scrum"></i></a>
       </div>
-      <br>
       <br>
       <div id="pdf">
       <a href="./cv.pdf" target="_blank">Puedes revisar mi CV aquí<i class="fas fa-file-alt"></i></a>
